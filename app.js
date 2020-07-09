47fdb5d59f73706bf78d7540d8079805f0609070
Jordan Burch <Jordanburch101@gmail.com>
2020-07-09 18:43:07 +1200

    Fixed street address on modal

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ function displayModal(index) {
             <p class="city">${city}</p>
             <hr>
             <p class="phone">${phone}​</p>
-            <p class="address">${street.number}, ${street.name}, ${state}, ${postcode}</p>
+            <p class="address">${street.number} ${street.name}, ${state}, ${postcode}</p>
             <p class="birthday">Birthday: 
             ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
